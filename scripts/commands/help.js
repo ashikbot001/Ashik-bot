fbf55b57d0f412b31651f7b436df7b5e9d79944c
ashikbot001 <bossagor745@gmail.com>
2024-10-05 02:37:43 +0600

    Update help.js

--- scripts/commands/help.js
+++ scripts/commands/help.js
@@ -90,7 +90,7 @@ let path = __dirname + `/cache/help.png`;
 
 
     const text = `╰➤ 𝗧𝗢𝗧𝗔𝗟 𝗖𝗢𝗠𝗠𝗔𝗡𝗗 ${arrayInfo.length}\n𝗨𝗦𝗘【 ${prefix}Help 】𝐏𝐀𝐆𝐄 𝐍𝐎.\n╰➤ «𝐍𝐔𝐌𝐁𝐄𝐑 𝐎𝐅 𝐏𝐀𝐆𝐄» ${page}
-╰➤ 𝐁𝐨𝐭 𝐀𝐝𝐦𝐢𝐧:BOSS~ASHIK\n𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤:https://www.facebook.com/mdjubaet.ahmed.9?mibextid=ZbWKwL\n𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩:wa.me/+8801709045888\n𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦:@mdjubaedahmed`;
+╰➤ 𝐁𝐨𝐭 𝐀𝐝𝐦𝐢n:BOSS~ASHIK\n𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤:https://www.facebook.com/profile.php?id=100095089282395&mibextid=ZbWKwL/md ashiket.ahmed.9?mibextid=ZbWKwL\n𝐖𝐡𝐚𝐭𝐬𝐚𝐩𝐩:wa.me/+8801647711001\n𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦:@;
     return api.sendMessage(`╭──«~»──CMD──«~»──╮\n\nPAGE ${page}/${Math.ceil(arrayInfo.length/numberOfOnePage)}` + "\n" + msg + "\n" + text, threadID, async (error, info) => {
       if (autoUnsend) {
         await new Promise(resolve => setTimeout(resolve, delayUnsend * 100000));
